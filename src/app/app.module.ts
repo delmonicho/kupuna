dec4e1ec411821179399f9cd12d495d82a5c8b4c
Nicho Delmo <ndelmoral13@gmail.com>
2022-04-28 21:49:11 -0700

    add expense tracker page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExpenseTrackerComponent } from './pages/expense-tracker/expense-tracker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseTrackerComponent
   ],
   imports: [
     BrowserModule
